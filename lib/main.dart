818d8bc8030081e3d0b3caf3bd1dac839ce412bf
Sidd Rao <siddrao.926@gmail.com>
2022-11-27 00:01:04 -0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Portfolio());
+}
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio',
+      theme: ThemeData.dark(),
+      home: null,
+    );
+  }
+}
