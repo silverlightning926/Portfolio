c4894faad7c60d613cb1cb2e29000f747779e0e2
Sidd Rao <siddrao.926@gmail.com>
2022-11-27 22:01:52 -0800

    Font Size Changes

--- lib/components/HomePage.dart
+++ lib/components/HomePage.dart
@@ -52,7 +52,7 @@ class HomePage extends StatelessWidget {
             child: Text(
               "> Learn More",
               style: GoogleFonts.mPlus1Code().copyWith(
-                fontSize: 20,
+                fontSize: 17,
                 color: const Color(0xFF4AF626),
               ),
             ),
